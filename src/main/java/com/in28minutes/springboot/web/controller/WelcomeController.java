906d5f364e2ba434a32927206c34fecfb47fe52d
Anuj Kumar Tiwari <anujkumar.tiwari@gmail.com>
2021-05-10 11:09:48 +0530

    name is shahenshah

--- src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/WelcomeController.java
@@ -13,7 +13,7 @@ public class WelcomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showWelcomePage(ModelMap model) {
 		model.put("name", getLoggedinUserName());
-		return "welcome";
+		return "welcome Shahenshah";
 	}
 
 	private String getLoggedinUserName() {
